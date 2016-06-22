994629e0dd042c7bcd15041d560fedc0c383fdbf
Curtis M. Humphrey, Ph.D <curtis@createdwithflair.com>
2016-06-22 10:30:13 -0500

    updated to use redux_helpers

---
+++ redux/files/__root__/redux/modules/__name__/__name___simple.js
@@ -0,0 +1,47 @@
+import Immutable from 'seamless-immutable'
+// import { createSelector } from 'reselect'
+import {
+  make_simple_selectors,
+  make_reducer_n_actions,
+} from 'redux_helpers'
+
+// -------
+// Initial State
+// --------
+
+const initial_state = {
+}
+
+// -------
+// Selectors
+// --------
+const BASE = '<%= snakeEntityName %>'
+export {BASE as BASE_SELECTOR_PATH}
+
+const simple_selectors = make_simple_selectors(initial_state, BASE)
+
+export const selectors = {
+  ...simple_selectors,
+}
+
+// ------------------------------------
+// Reducer and Actions
+// ------------------------------------
+const action_types_prefix = '<%= snakeEntityName %>'
+
+const public_handlers = {
+  reset: () => Immutable(initial_state),
+}
+
+const private_handlers = {
+}
+
+export const {reducer, private_actions, actions, ACTION_TYPES} = make_reducer_n_actions({
+  public_handlers,
+  private_handlers,
+  action_types_prefix,
+  initial_state,
+  Immutable,
+})
+export default reducer
+
