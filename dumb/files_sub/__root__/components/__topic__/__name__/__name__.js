c241889a0082330cc0bdfe00d4b106a962c4d94a
Curtis M. Humphrey, Ph.D <curtis@createdwithflair.com>
2017-01-09 09:37:06 -0600

    Added be topic flag

---
+++ dumb/files_sub/__root__/components/__topic__/__name__/__name__.js
@@ -0,0 +1,19 @@
+import React, { PropTypes } from 'react'
+import CSSModules from 'react-css-modules'
+
+import styles from './<%= pascalEntityName %>.scss'
+
+class <%= pascalEntityName %> extends React.PureComponent {
+  static propTypes = {
+  };
+  static defaultProps = {
+  };
+
+  render () {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default CSSModules(<%= pascalEntityName %>, styles)
--- dumb/index.js
+++ dumb/index.js
@@ -1,5 +1,30 @@
+const path = require('path')
+
+var has_topic = false
+
 module.exports = {
   description () {
-    return 'generates a dumb (pure) component'
-  }
+    return 'generates a dumb (pure) component, --topic=`subfolder`'
+  },
+
+  beforeInstall: function (options) {
+    has_topic = options.entity.options.topic != null
+  },
+
+  fileMapTokens: function () {
+    // Return custom tokens to be replaced in your files
+    return {
+      __topic__: function (options) {
+        // logic to determine value goes here
+        return options.entity.options.topic || 'unknown'
+      },
+    }
+  },
+
+  filesPath: function () {
+    if (has_topic) {
+      return path.join(this.path, 'files_sub')
+    }
+    return path.join(this.path, 'files')
+  },
 }
